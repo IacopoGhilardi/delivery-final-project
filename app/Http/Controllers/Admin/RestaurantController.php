d2cd11c3f49b77240aa24e43121e525c61403fdc
Giulio <giuliovolletti@hotmail.com>
2021-03-12 17:17:28 +0100

    controller restaurant

---
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Restaurant;
+use Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::where('user_id', Auth::id())->get();
+
+        return view('admin.deliverboo.index', compact('restaurants'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.deliverboo.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $data['user_id'] = Auth::id();
+        $data['slug'] = Str::slug($data['business_name']);
+        $data['img_path'] = Storage::disk('public')->put('images', $data['img_path']);
+
+        $newRestaurant = new Restaurant();
+        $newRestaurant->fill($data);
+        $newRestaurant->save();
+        
+        return redirect()->route('admin.deliverboo.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/deliverboo/index.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Index</title>
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+  <div class="container">
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th scope="col"> Nome Attività </th>
+          <th scope="col"> Indirizzo  </th>
+          <th scope="col"> PIVA </th>
+          <th style="width: 150px"> Immagine </th>
+          <th colspan="3"></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($restaurants as $restaurant)
+          <tr>
+            <td> {{$restaurant->business_name}} </td>
+            <td> {{$restaurant->address}} </td>
+            <td> {{$restaurant->PIVA}} </td>
+            <td> <img src="{{ asset('storage/' . $restaurant->img_path) }}" alt="{{$restaurant->business_name}}"> </td>
+            <td>  <a href="btn btn-info"></a> </td>
+            <td>  <a href="btn btn-success"></a> </td>
+            <td>  <a href="btn btn-danger"></a> </td>
+          </tr>          
+        @endforeach      
+      </tbody>
+  
+    </table>
+
+  </div>
+  
+
+</body>
+</html>
