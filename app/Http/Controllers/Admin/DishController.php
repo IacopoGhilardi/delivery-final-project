cdcf16a57a8987c9bd7979b866c6bdf614e56a0c
Giulio <giuliovolletti@hotmail.com>
2021-03-15 17:10:05 +0100

    controller dishes

--- app/Http/Controllers/Admin/DishController.php
+++ app/Http/Controllers/Admin/DishController.php
@@ -4,15 +4,94 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Dish;
+use Auth;
 
 class DishController extends Controller
 {
-    //
-    public function show() {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       $dishes = Dish::where('restaurant_id', Auth::id())->get();
+
+       return view('admin.menu.index', compact('dishes'));
 
     }
 
-    public function createDish() {
-        
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.menu.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['restaurant_id'] = Auth::id();
+        $newDish = new Dish();
+
+        $newDish->fill($data);
+        $newDish->save();
+
+        return redirect()->route('admin.menu.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
