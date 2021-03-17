20679e5cd77410354170b0e482fc1afae9de39f3
francescofogliajj <francescofogliajj@gmail.com>
2021-03-17 13:28:31 +0100

    old() in menu/create & restaurant/create

--- resources/views/admin/restaurant/create.blade.php
+++ resources/views/admin/restaurant/create.blade.php
@@ -18,33 +18,33 @@
         <div class="form-group row">
           <label class="col-sm-2 col-form-label col-form-label-lg" for="business_name">Nome Ristorante</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" name="business_name" id="business_name"  value="">
+            <input type="text" class="form-control" name="business_name" id="business_name" value="{{ old('business_name') }}">
           </div>
         </div>
     
         <div class="form-group row">
           <label class="col-sm-2 col-form-label col-form-label-lg" for="address">Indirizzo</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" name="address" id="address"  value="">
+            <input type="text" class="form-control" name="address" id="address" value="{{ old('address') }}">
           </div>
         </div>
     
         <div class="form-group row">
           <label class="col-sm-2 col-form-label col-form-label-lg" for="PIVA">PIVA</label>
           <div class="col-sm-10">
-            <input type="text" class="form-control" name="PIVA" id="PIVA"  value="">
+            <input type="text" class="form-control" name="PIVA" id="PIVA" value="{{ old('PIVA') }}">
           </div>
         </div>
     
         <div class="form-group row">
           <label class="col-sm-2 col-form-label col-form-label-lg " for="img_path">Immagine</label>
           <div class="col-sm-10">
-            <input type="file" class="" name="img_path" id="img_path"  accept="image/*">
+            <input type="file" class="" name="img_path" id="img_path" value="{{ old('img_path') }}" accept="image/*">
           </div>
         </div>
 
         <div class="form-group row">
-          <label for="address" class="col-sm-2 col-form-label col-form-label-lg">{{ __('Select cuisine style') }}</label>
+          <label for="address" class="col-sm-2 col-form-label col-form-label-lg">{{ __('Seleziona tipologia') }}</label>
 
           <div class="col-md-6">
               <select id="type_id" name="types[]" class="js-example-basic-multiple" multiple="multiple" style="width: 100%">
