45195816c4bdcf8fda348d8fc6b8adaf5d9fc1bc
francescofogliajj <francescofogliajj@gmail.com>
2021-03-17 17:10:25 +0100

    Info ristoranti

--- resources/views/admin/restaurant/show.blade.php
+++ resources/views/admin/restaurant/show.blade.php
@@ -14,7 +14,7 @@
                 @else
                     <img class="img-fluid" src="{{ asset('images/restaurantDefault.png') }}" alt="{{ $restaurant->business_name }}">
                 @endif
-                <h2 class="my-2">INFO</h2>
+                <h2 class="mt-4">INFO</h2>
                 <div class="card mt-2">
                     <div class="card-body">
                         <h2>{{ $restaurant->business_name }}</h2>
