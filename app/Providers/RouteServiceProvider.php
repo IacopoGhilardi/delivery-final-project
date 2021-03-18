ef9333838f719805ffde9336ef913f51b5c3ece8
Iacopo <iacopo.ghila@gmail.com>
2021-03-18 12:31:47 +0100

    cambiata routeServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'guest/deliverboo';
+    public const HOME = 'deliverboo';
 
     /**
      * Define your route model bindings, pattern filters, etc.
