47394fb5665904e1faa990540c3bf9ce4de3f9fa
Riccardo Ferrari <richi.f93@gmail.com>
2021-03-23 12:34:08 +0100

    fix cart

--- resources/views/guest/homepage.blade.php
+++ resources/views/guest/homepage.blade.php
@@ -29,23 +29,16 @@
                                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                         {{ Auth::user()->firstName }}
                                     </a>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('admin.restaurant.index') }}">I miei Ristoranti</a>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                                @endguest
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+            
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </nav>
             </div>
             <div class="custom-shape-divider-bottom-1616418170">
                 <div class="image_container">
@@ -188,26 +181,30 @@
                     </div>
                 </div>
             </div>
-            {{-- TUTTI I RISTORANTI --}}
-            <div class="all_restaurants" v-if="firstSearch && filteredRestaurants.length == 0">
-                <div class="restaurants_wrapper">
-                    {{-- <h2 v-if="filteredRestaurants.length > 1">@{{filteredRestaurants.length}} Ristoranti</h2>
-                    <h2 v-if="filteredRestaurants.length == 1">@{{filteredRestaurants.length}} Ristorante</h2> --}}
-                    <div class="restaurants_container">
-                        <div v-for="restaurant in allRestaurants" class="restaurant">
-                            <form action="{{ route('guest.restaurant.show') }}" method="post">
-                                @csrf
-                                @method('POST')
-                                <input name="business_name" type="hidden" :value="restaurant.business_name">
-                                <div class="restaurant_image_box">
-                                    <img :src="`../storage/${restaurant.img_path}`" alt="">
-                                </div>
-                                <div class="restaurant_info">
-                                     <p>@{{restaurant.business_name}}</p>
-                                </div>
-                                <button type="submit"></button>
-                            </form>
-                        </div>
+        </div>
+        {{-- TUTTI I RISTORANTI --}}
+        <div class="all_restaurants" v-if="firstSearch && filteredRestaurants.length == 0">
+            <div class="restaurants_wrapper">
+                {{-- <h2 v-if="filteredRestaurants.length > 1">@{{filteredRestaurants.length}} Ristoranti</h2>
+                <h2 v-if="filteredRestaurants.length == 1">@{{filteredRestaurants.length}} Ristorante</h2> --}}
+                <div class="restaurants_container">
+                    <div v-for="restaurant in allRestaurants" class="restaurant">
+                        <form action="{{ route('guest.restaurant.show') }}" method="post">
+                            @csrf
+                            @method('POST')
+                            <input name="business_name" type="hidden" :value="restaurant.business_name">
+                            <div class="restaurant_image_box">
+                                <img :src="`../storage/${restaurant.img_path}`" alt="">
+                            </div>
+                            <div class="restaurant_info">
+                                 <p class="businnes_name">@{{restaurant.business_name}}</p>
+                                 <ul>
+                                    <li class="badge badge-secondary mx-1" v-for="type in restaurant.types">@{{type.name}}</li>
+                                 </ul>
+                                 <span><i class="fas fa-map-marker-alt"></i> @{{restaurant.address}}</span>
+                            </div>
+                            <button type="submit"></button>
+                        </form>
                     </div>
                 </div>
             </div>
