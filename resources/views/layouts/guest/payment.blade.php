b788d838b4feee83e368b800c222aa9d6df9c17a
Iacopo <iacopo.ghila@gmail.com>
2021-03-27 18:31:56 +0100

    Aggiunta icona scheda

--- resources/views/layouts/guest/payment.blade.php
+++ resources/views/layouts/guest/payment.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="{{ asset('images/logo/deliveboo-resp-black.png') }}">
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/paymentLayout.css') }}">
