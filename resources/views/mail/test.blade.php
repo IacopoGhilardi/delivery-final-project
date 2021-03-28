4091b0aa817bda499a3ae03eb016e7f149a28796
francescofogliajj <francescofogliajj@gmail.com>
2021-03-29 00:54:44 +0200

    style in mail/test.php

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -5,21 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>DeliveBoo</title>
+    <style type="text/css">
+
+        * {
+            box-sizing: border-box
+            margin: 0;
+            padding: 0;
+        }
+
+        header {
+            width: 100%;
+            height: 50px;
+        }
+
+        img {
+            width: 200px;
+        }
+
+        main {
+            width: 100%;
+            height: calc(100vh - 50px);
+            color: white;
+            background-image: url({{ asset('images/logo/home-img.png') }});
+            background-size: cover;
+            backround-position: center;
+            position: relative;
+        }
+
+        div {
+            width: 100%;
+            height: calc(100vh - 50px);
+            background-color: rgba(0,0,0,0.2);
+            position: absolute;
+            top: 0;
+            left: 0;
+            text-align: center;
+        }
+
+    </style>
 </head>
 <body>
-    {{-- <div style="width: 100%; height: 100vh; padding: 20px; color: white; background: linear-gradient(153deg, #f8585e 20%, #f8b64a 80%)">
-        <h1>DELIVEBOO</h1>
-        <h2>Ordine effettuato correttamente</h2>
-        <h3>Segui i dettagli nel sito</h3>
-    </div> --}}
     
-    <header style="width: 100%; background-color: white">
-        <img style="width: 200px" class="img-fluid" src="{{ asset('images/logo/logo-black.png') }}" alt="logo">
+    <header>
+        <img class="img-fluid" src="{{ asset('images/logo/logo-black.png') }}" alt="logo">
     </header>
 
-    <main style="width: 100%; height: calc(100vh - 50px); padding: 20px; color: white; background-image: url({{ asset('images/logo/home-img.png') }}); background-size: cover; background-position: top">
-        <h1>Ordine effettuato correttamente</h1>
-        <h2>Segui i dettagli nel sito</h2>
+    <main>
+        <div>
+            <h1>Ordine effettuato correttamente</h1>
+            <h2>Segui i dettagli nel sito</h2>
+        </div>
     </main>
 
 </body>
